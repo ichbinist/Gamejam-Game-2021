774ac3927bc5f1035830561e614dec722624247b
Oğuzhan Arar <ichbinizm@gmail.com>
2021-01-30 16:06:13 +0300

    Bug Fix

--- Assets/Base/Script/Interactable/DoorInteractable.cs
+++ Assets/Base/Script/Interactable/DoorInteractable.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class DoorInteractable : ColliderInteractable
 {
+    public DoorInteractable LinkedDoor;
     public Room AttachedRoom;
+
     public override void Do()
     {
         base.Do();
         if(RoomManager.Instance.CurrentRoom != null)
             RoomManager.Instance.OnRoomExit.Invoke(RoomManager.Instance.CurrentRoom.RoomID);
-        RoomManager.Instance.OnRoomEnter.Invoke(AttachedRoom.RoomID);           
+        RoomManager.Instance.OnRoomEnter.Invoke(AttachedRoom.RoomID);
+        TeleportPlayer();
+    }
+
+    public void TeleportPlayer()
+    {
+        if(LinkedDoor != null)
+            CharacterManager.Instance.GetPlayer.transform.position = LinkedDoor.transform.position;
     }
 }
