9d4a342ab3ae558e844ee49257351078c8977f20
Oğuzhan Arar <ichbinizm@gmail.com>
2021-01-30 03:56:43 +0300

    Scripts are created

---
+++ Assets/Base/Script/Room Management/RoomManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class RoomManager : MonoBehaviour
+{
+    public RoomEvent OnRoomEnter = new RoomEvent();
+    public RoomEvent OnRoomExit = new RoomEvent();
+
+    private void OnEnable()
+    {
+      if(!Managers.Instance) return;
+
+    }
+
+    private void OnDisable()
+    {
+      if(!Managers.Instance) return;
+
+    }
+}
+public class RoomEvent : UnityEvent<int> { }
