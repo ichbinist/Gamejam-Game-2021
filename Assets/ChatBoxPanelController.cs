ef56acb57d5474c12873b0f68ac0f18f42a925c4
Mustafa CAN <mcqn14@gmail.com>
2021-01-30 03:56:10 +0300

    insights are done but chatbox needs to be finished

---
+++ Assets/ChatBoxPanelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChatBoxPanelController : MonoBehaviour
+{
+    public Transform chatBox;
+    private TextMeshProUGUI chatBoxText;
+    public TextMeshProUGUI ChatBoxText { get { return (chatBoxText == null) ? chatBoxText = GameObject.FindGameObjectWithTag("chatBox").GetComponentInChildren<TextMeshProUGUI>() : chatBoxText; } }
+
+    private void OnEnable()
+    {
+        if (!Managers.Instance) return;
+        ChatManager.Instance.onStartCommunication.AddListener(onStartCommunicationListener);
+    }
+
+    private void OnDisable()
+    {
+        if (!Managers.Instance) return;
+        ChatManager.Instance.onStartCommunication.AddListener(onStartCommunicationListener);
+    }
+
+    private void onStartCommunicationListener()
+    {
+        chatBox.DOMoveY(chatBox.position.y + 80f, 0.2f);
+        chatBoxText.text = "Lahmacun sever misin?";
+    } 
+}
---
+++ Assets/Scripts/InsightPanelController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class InsightPanelController : MonoBehaviour
+{
+    public Transform insightBox;
+    private TextMeshProUGUI insightText;
+    public TextMeshProUGUI InsightText { get { return (insightText == null) ? insightText = GameObject.FindGameObjectWithTag("insightBox").GetComponentInChildren<TextMeshProUGUI>() : insightText; } }
+    bool isAnimationStarted = false;
+
+    private void OnEnable()
+    {
+        if (!Managers.Instance) return;
+        ChatManager.Instance.onStartCommunication.AddListener(onStartCommunicationListener);
+    }
+
+    private void OnDisable()
+    {
+        if (!Managers.Instance) return;
+        ChatManager.Instance.onStartCommunication.RemoveListener(onStartCommunicationListener);
+    }
+
+    private void onStartCommunicationListener()
+    {
+        int tweenID = gameObject.GetInstanceID();
+        if (isAnimationStarted)
+            return;
+        insightBox.DOMoveY(insightBox.position.y + 50f, 0.2f).SetId(tweenID)
+            .OnStart(() =>
+        {
+            if (!isAnimationStarted)
+            {
+                isAnimationStarted = true;
+            }
+        });
+        InsightText.text = "Pil bulmalıyım";
+        StartCoroutine(startCO());
+    }
+
+    IEnumerator startCO()
+    {
+        yield return new WaitForSeconds(2f);
+        insightBox.DOMoveY(insightBox.position.y-50f, 0.2f).SetId(gameObject.GetInstanceID()).OnComplete(() =>
+        {
+            isAnimationStarted = false;
+        });
+    }
+}
